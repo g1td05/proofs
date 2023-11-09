f33bba5c2be4e79fbc2a0bbe2a70059b9c3fc1bc
DOS <diego.os@gmail.com>
2023-11-09 17:03:51 -0600

    Correccion de raiz.	DOS

--- first-rest-api/src/main/java/com/dos/springboot/firstrestapi/survey/SurveyResource.java
+++ first-rest-api/src/main/java/com/dos/springboot/firstrestapi/survey/SurveyResource.java
@@ -2,18 +2,14 @@ package com.dos.springboot.firstrestapi.survey;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SurveyResource {
 
-	@Autowired
 	private SurveyService surveyService;
 	
-	public SurveyResource() {}
-
 	public SurveyResource(SurveyService surveyService) {
 		super();
 		this.surveyService = surveyService;
